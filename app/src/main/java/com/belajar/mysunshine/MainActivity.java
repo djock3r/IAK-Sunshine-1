d0e69e839a3507e98c087f087126eb5cd414f7af
Wira Syahputra <th3_djock3r@yahoo.com>
2017-04-29 19:27:13 +0700

    Muncul Data Dummy

--- app/src/main/java/com/belajar/mysunshine/MainActivity.java
+++ app/src/main/java/com/belajar/mysunshine/MainActivity.java
@@ -3,12 +3,17 @@ package com.belajar.mysunshine;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.placeholderfragment);
+        setContentView(R.layout.activity_main);
+
+
     }
 }
 
